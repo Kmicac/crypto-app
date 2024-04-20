438f13b31bf5f17ac0cf861f7f91e77de5cfb156
Camilo <camilojpg30@gmail.com>
2024-04-20 06:06:17 -0300

    adding CirclesGraph

--- src/layout/CryptoDashboard.tsx
+++ src/layout/CryptoDashboard.tsx
@@ -1,5 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import * as d3 from 'd3';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 // import axios from 'axios';
 
 // interface CryptoData {
@@ -10,117 +10,142 @@
 //   market_cap: number;
 // }
 
-// const CryptoViz: React.FC = () => {
-//   const [cryptoData, setCryptoData] = useState<CryptoData[]>([]);
+const CirclesGraph: React.FC = () => {
+  const svgRef = useRef<HTMLDivElement>(null);
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const response = await axios(
-//         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en'
-//       );
-//       const data: CryptoData[] = await response.data;
-//       setCryptoData(data);
-//     };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await axios
+  //       (
+  //         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1'
+  //       );
+  //     const data: CryptoData[] = response.data;
+   
+  //   };
 
-//     fetchData();
-//   }, []);
+  //   fetchData();
+  // }, []);
 
-//   useEffect(() => {
-//     if (cryptoData.length > 0) {
-//       drawChart(cryptoData);
-//     }
-//   }, [cryptoData]);
+  useEffect(() => {
+    if (svgRef.current) {
 
-//   const drawChart = (data: CryptoData[]) => {
-//     const width = 460;
-//     const height = 460;
-//     const svg = d3
-//       .select('#crypto-viz')
-//       .append('svg')
-//       .attr('width', width)
-//       .attr('height', height);
+      // configura las dimenciones y margenes dde la grafica de los circulos
+      const width = svgRef.current.clientWidth;
+      const height = svgRef.current.clientHeight;
 
-//     const color = d3.scaleOrdinal().range(d3.schemeSet1);
-//     const size = d3.scaleLinear().range([7, 55]);
+      // Limpiar el área de visualización antes de agregar nuevos elementos
+      d3.select("#my_dataviz").selectAll("*").remove();
 
-//     const Tooltip = d3
-//       .select('#crypto-viz')
-//       .append('div')
-//       .style('opacity', 0)
-//       .attr('class', 'tooltip')
-//       .style('background-color', 'white')
-//       .style('border', 'solid')
-//       .style('border-width', '2px')
-//       .style('border-radius', '5px')
-//       .style('padding', '5px');
+      // Read data
+      d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/11_SevCatOneNumNestedOneObsPerGroup.csv").then((data: any[]) => {
+        // Filter a bit the data -> more than 1 million inhabitants
+        data = data.filter((d: any) => d.value > 10000000);
 
-//     const mousemove = (event: React.MouseEvent<SVGCircleElement, MouseEvent>, d: CryptoData) => {
-//       const coords = d3.pointer(event, svg.node());
-//       Tooltip.style('opacity', 1)
-//         .html(
-//           `<u>${d.name}</u><br>Symbol: ${d.symbol}<br>Price: $${d.current_price}`
-//         )
-//         .style('left', `${coords[0] + 20}px`)
-//         .style('top', `${coords[1]}px`);
-//     };
+        // Color palette for continents
+        const color = d3.scaleOrdinal<string>()
+          .domain(["Asia", "Europe", "Africa", "Oceania"])
+          .range(d3.schemeSet1);
 
-//     const mouseleave = () => {
-//       Tooltip.style('opacity', 0);
-//     };
+        // Size scale for countries
+        const size = d3.scaleLinear<number, number>()
+          .domain([0, 1400000000])
+          .range([7, 55]); // circle will be between 7 and 55 px wide
 
-//     const node = svg.append('g')
-//       .selectAll('circle')
-//       .data(data)
-//       .enter()
-//       .append('circle')
-//       .attr('class', 'node')
-//       .attr('r', (d) => size(d.market_cap) as number)
-//       .attr('cx', width / 2)
-//       .attr('cy', height / 2)
-//       // .style('fill', (d, i) => color(i) as string)
-//       .style('fill-opacity', 0.8)
-//       .attr('stroke', 'black')
-//       .style('stroke-width', 1)
-//       .on('mousemove', mousemove)
-//       .on('mouseleave', mouseleave)
-//       .call(d3.drag()
-//         .on('start', dragstarted)
-//         .on('drag', dragged)
-//         .on('end', dragended));
+        // create a tooltip
+        const Tooltip = d3.select("#my_dataviz")
+          .append("div")
+          .style("opacity", 0)
+          .attr("class", "tooltip")
+          .style("background-color", "white")
+          .style("border", "solid")
+          .style("border-width", "2px")
+          .style("border-radius", "5px")
+          .style("padding", "5px");
 
-//     const simulation = d3.forceSimulation()
-//       .force("center", d3.forceCenter().x(width / 2).y(height / 2)) 
-//       .force("charge", d3.forceManyBody().strength(.1)) 
-//       .force("collide", d3.forceCollide().strength(.2).radius(function (d) { return (size(d.value) + 3) }).iterations(1)) 
+        // Three function that change the tooltip when user hover / move / leave a cell
+        const mouseover = function () {
+          Tooltip.style("opacity", 0);
+        }
+        const mousemove = function (event: any, d: any) {
+          Tooltip
+            .html('<u>' + d.key + '</u>' + "<br>" + d.value + " inhabitants")
+            .style("left", (event.x / 2 + 20) + "px")
+            .style("top", (event.y / 2 - 30) + "px");
+        }
+        const mouseleave = function () {
+          Tooltip.style("opacity", 0);
+        }
 
+        // Initialize the circle: all located at the center of the svg area
+        const svg = d3.select("#my_dataviz")
+          .append("svg")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .attr("viewBox", `0 0 ${width} ${height}`)
+          .attr("preserveAspectRatio", "xMidYMid meet");
 
-//       simulation
-//       .nodes(data)
-//       .on("tick", function(d) {
-//         node
-//             .attr("cx", d => d.x)
-//             .attr("cy", d => d.y)
-//       });
+        const node = svg.selectAll<SVGCircleElement, any>("circle")
+          .data(data)
+          .enter().append("circle")
+          .attr("class", "node")
+          .attr("r", (d: any) => size(d.value)!)
+          .attr("cx", width / 2)
+          .attr("cy", height / 2)
+          .style("fill", (d: any) => color(d.region))
+          .style("fill-opacity", 0.8)
+          .attr("stroke", "black")
+          .style("stroke-width", 1)
+          .on("mouseover", mouseover)
+          .on("mousemove", mousemove)
+          .on("mouseleave", mouseleave)
+          .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended) as any);
+            
+            // Características de las fuerzas aplicadas a los nodos.:
+            const simulation = d3.forceSimulation<any>()
+            .force("center", d3.forceCenter().x(width / 2).y(height / 2))
+            .force("charge", d3.forceManyBody().strength(0.1))
+            .force("collide", d3.forceCollide().strength(0.2).radius((d: any) => (size(d.value)! + 3)).iterations(1));
+            
+           
+            simulation.nodes(data)
+              .on("tick", function () {
+                node.attr("cx", (d: any) => {
+                  // Limita la posición x para permanecer dentro del área de visualización
+                  d.x = Math.max(size(d.value)! / 2, Math.min(width - size(d.value)! / 2, d.x));
+                  return d.x;
+                })
+                  .attr("cy", (d: any) => {
+                    // Limita la posición y para permanecer dentro del área de visualización
+                    d.y = Math.max(size(d.value)! / 2, Math.min(height - size(d.value)! / 2, d.y));
+                    return d.y;
+                  });
+              });
 
-//       function dragstarted(event, d) {
-//         if (!event.active) simulation.alphaTarget(.03).restart();
-//         d.fx = d.x;
-//         d.fy = d.y;
-//       }
-//       function dragged(event, d) {
-//         d.fx = event.x;
-//         d.fy = event.y;
-//       }
-//       function dragended(event, d) {
-//         if (!event.active) simulation.alphaTarget(.03);
-//         d.fx = null;
-//         d.fy = null;
-//       }
-    
-//     }
-//   };
+        // 
+        function dragstarted(event: any, d: any) {
+          if (!event.active) simulation.alphaTarget(0.03).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+        function dragged(event: any, d: any) {
+          d.fx = event.x;
+          d.fy = event.y;
+        }
+        function dragended(event: any, d: any) {
+          if (!event.active) simulation.alphaTarget(0.03);
+          d.fx = null;
+          d.fy = null;
+        }
+      });
+    }
+  }, []);
 
-//   return <div id="crypto-viz"></div>;
-// };
+  return (
+    <div id="my_dataviz" ref={svgRef}></div>
+  );
+}
 
-// export default CryptoViz;
+export default CirclesGraph;
--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -3,32 +3,54 @@ import logo from '../assets/MoralisMoneyLogomark.5b65b0d7.svg'
 import logo1 from '../assets/MoralisMoneyLogotype.431faf8b.svg'
 import { FaChartBar, FaChevronDown, FaSearch, FaBookmark, FaGraduationCap, FaBriefcase, FaCode, FaCog, FaChevronLeft, FaArrowRight } from 'react-icons/fa';
 import SearchBar from './SearchBar';
+import CirclesGraph from './CryptoDashboard';
 
 export function PageHeader() {
   const [open, setOpen] = useState(true);
+  const [showSubsections, setShowSubsections] = useState(false);
 
   const handleToggleSidebar = () => {
     setOpen(!open);
   }
 
+  const handleToggleSubsections = () => {
+    setShowSubsections(!showSubsections);
+  }
   return (
-<div className='flex'>
-  <div className={`flex flex-col ${open ? "w-64" : "w-18 gap-6"} h-screen bg-sky-950 bg-opacity-50 transition-all duration-300 pt-1`}>
-    <div className='px-6 pt-6 mb-6'>
-      <a className='flex gap-3' href="/">
-        <img src={logo} className={`h-8 ${!open ? 'absolute pt-0' : 'flex'}`} />
-        <img src={logo1} className={`h-6 ${!open && 'hidden'}`} />
-      </a>
-    </div>
-    <div className='flex flex-col justify-content min-h-40 overflow-y-auto py-2 px-4 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-sky-950'>
-      <ul className='flex flex-col justify-content'>
-            <button className='flex justify-between text-[#f0f8ff]  w-full h-16 px-4 rounded-lg hover:bg-[#0d2035] hover:shadow-md'>
+    <div className='flex'>
+      <div className={`flex flex-col ${open ? "w-64" : "w-18 gap-6"} h-screen bg-sky-950 bg-opacity-50 transition-all duration-300 pt-1`}>
+        <div className='px-6 pt-6 mb-6'>
+          <a className='flex gap-3' href="/">
+            <img src={logo} className={`h-8 ${!open ? 'absolute pt-0' : 'flex'}`} />
+            <img src={logo1} className={`h-6 ${!open && 'hidden'}`} />
+          </a>
+        </div>
+        <div className='flex flex-col justify-content min-h-40 overflow-y-auto py-2 px-4 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-sky-950'>
+          <ul className='flex flex-col justify-content'>
+            <button onClick={handleToggleSubsections} className='flex justify-between text-[#f0f8ff]  w-full h-16 px-4 rounded-lg hover:bg-[#0d2035] hover:shadow-md'>
               <li className="flex items-center gap-4 pt-6">
                 <FaChartBar className='text-white' />
                 <span className={` ${!open && 'hidden'}`}>Market Overview</span>
                 <FaChevronDown className={` w-3 h-3 ${!open && 'hidden'}`} />
               </li>
             </button>
+            {showSubsections && (
+          <>
+            {/* Agrega tus dos secciones adicionales aquí */}
+            <a className='flex justify-between text-[#f0f8ff]  w-full h-16 px-4 rounded-lg hover:bg-[#0d2035] hover:shadow-md' >
+              <li className="flex items-center gap-4 pt-6">
+                {/* Icono de la sección 1 */}
+                <span className={`${!open && 'hidden'}`}>Top token by market cap</span>
+              </li>
+            </a>
+            <a className='flex justify-between text-[#f0f8ff]  w-full h-16 px-4 rounded-lg hover:bg-[#0d2035] hover:shadow-md' >
+              <li className="flex items-center gap-4 pt-6">
+                {/* Icono de la sección 2 */}
+                <span className={`${!open && 'hidden'}`}>Top token on the move</span>
+              </li>
+            </a>
+          </>
+        )}
             <button className='flex justify-between text-[#f0f8ff]  w-full h-16 px-4 rounded-lg hover:bg-[#0d2035] hover:shadow-md' >
               <li className="flex items-center gap-4 pt-6">
                 <FaSearch className='text-white' />
@@ -70,21 +92,24 @@ export function PageHeader() {
           </ul>
         </div>
         <div className={`flex flex-col items-center justify-center min-h-24 ${!open && 'hidden'}`}>
-       <div className='pt-6 pb-0'>
+          <div className='pt-6 pb-0'>
             <button className={`flex justify-center w-56 px-4 py-3 text-sm font-medium text-gray-400 border-2 border-gray-600 rounded-lg gap-2 outline-none bg-transparent hover:text-gray-200 hover:border-gray-400 transition-colors duration-150 ${!open && 'hidden'}`}>
-            <FaArrowRight className="h-4 w-4 text-Teal-800" />
+              <FaArrowRight className="h-4 w-4 text-Teal-800" />
               Start Trial
-            </button>
-       </div>
-      </div>
-          <div className="mt-auto px-8 py-4 flex justify-end border-y border-slate-700">
-            <button onClick={handleToggleSidebar}>
-              <FaChevronLeft className={`${open ? '' : 'rotate-180'} transition-transform duration-300`} />
             </button>
           </div>
         </div>
+        <div className="mt-auto px-8 py-4 flex justify-end border-y border-slate-700">
+          <button onClick={handleToggleSidebar}>
+            <FaChevronLeft className={`${open ? '' : 'rotate-180'} transition-transform duration-300`} />
+          </button>
+        </div>
+      </div>
       <div className='flex-1'>
         <SearchBar />
+        <div>
+          <CirclesGraph />
+        </div>
       </div>
     </div>
   )
